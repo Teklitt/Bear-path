b2387e526530189666877d6ebcb76d9d8aa3d463
Teklitt <mebudenifemi@gmail.com>
2023-11-21 20:42:41 -0500

    Boiler plate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/BearPath Logo.png'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>BearPath</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.figma.com/file/r24wBhKeSLYUME1kP1DoTy/Product-Design%2FMockUp?node-id=0%3A1&mode=dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link to BearPath Figma Design
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
